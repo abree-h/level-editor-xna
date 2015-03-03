ce4a4d7ff468b6989fb72b49d34f23eb42b9749a
Abby Hipolito <abree.hipolito@gmail.com>
2015-03-03 14:29:20 -0800

    renamed to leveleditor

--- LevelEditor(XNA)/LevelEditor(XNA)/LevelEditor(XNA)/Program.cs
+++ LevelEditor(XNA)/LevelEditor(XNA)/LevelEditor(XNA)/Program.cs
@@ -10,7 +10,7 @@ namespace LevelEditor_XNA_
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (LevelEditor game = new LevelEditor())
             {
                 game.Run();
             }
